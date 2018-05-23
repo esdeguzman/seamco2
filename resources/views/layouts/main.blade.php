97578f9386a21924df029b598da325bafd7752ca
Daniel Bajana <it.danielbajana@gmail.com>
2018-05-23 19:19:43 +0800

    Add main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{ url('css/bootstrap-4.1.0.css') }}"crossorigin="anonymous">
+
+        <style>
+            body {
+                font-size: .875rem;
+                background-color: #eee;
+            }
+
+            .feather {
+                width: 16px;
+                height: 16px;
+                vertical-align: text-bottom;
+            }
+
+            /*
+            * Sidebar
+            */
+
+            .sidebar {
+                position: fixed;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                z-index: 100; /* Behind the navbar */
+                padding: 72px 0 0; /* Height of navbar */
+                box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+            }
+
+            .sidebar-sticky {
+                position: relative;
+                top: 0;
+                height: calc(100vh - 48px);
+                padding-top: .5rem;
+                overflow-x: hidden;
+                overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+            }
+
+            @supports ((position: -webkit-sticky) or (position: sticky)) {
+                .sidebar-sticky {
+                    position: -webkit-sticky;
+                    position: sticky;
+                }
+            }
+
+            .sidebar .nav-link {
+                font-weight: 500;
+                color: #333;
+            }
+
+            .sidebar .nav-link .feather {
+                margin-right: 4px;
+                color: #999;
+            }
+
+            .sidebar .nav-link.active {
+                color: #007bff;
+            }
+
+            .sidebar .nav-link:hover .feather,
+            .sidebar .nav-link.active .feather {
+                color: inherit;
+            }
+
+            .sidebar-heading {
+            font-size: .75rem;
+            text-transform: uppercase;
+            }
+
+            /*
+            * Content
+            */
+
+            [role="main"] {
+                padding-top: 72px; /* Space for fixed navbar */
+            }
+
+            /*
+            * Navbar
+            */
+
+            .navbar-brand {
+                padding-top: .75rem;
+                padding-bottom: .75rem;
+                font-size: 1rem;
+                background-color: rgba(0, 0, 0, .25);
+                box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
+            }
+
+            .navbar .form-control {
+                padding: .75rem 1rem;
+                border-width: 0;
+                border-radius: 0;
+            }
+
+            .form-control-dark {
+                color: #fff;
+                background-color: rgba(255, 255, 255, .1);
+                border-color: rgba(255, 255, 255, .1);
+            }
+
+            .form-control-dark:focus {
+                border-color: transparent;
+                box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
+            }
+
+            /*
+            * Utilities
+            */
+
+            .border-top { border-top: 1px solid #e5e5e5; }
+            .border-bottom { border-bottom: 1px solid #e5e5e5; }
+
+        </style>
+
+        @yield('styles')
+
+        <title>@yield('page-title')</title>
+    </head>
+    <body>
+
+        <!-- START HEADER -->
+        <div class="d-flex flex-column fixed-top flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow" style="background: #346cb0">
+            <h5 class="my-0 mr-md-auto text-light font-weight-normal">SEAMCO</h5>
+
+            <nav class="my-2 my-md-0 mr-md-3">
+                <a class="p-2 text-light" href="#">{{ auth()->user()->name }}</a>
+
+            </nav>
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
+                <span data-feather="log-out" style="color: white"></span>
+            </a>
+        </div>
+        <!-- END HEADER -->
+
+        <!-- START SIDEBAR -->
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div class="sidebar-sticky">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('dashboard') }}">
+                                    <span data-feather="home"></span>
+                                    Dashboard <span class="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="users"></span>
+                                    Members
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="credit-card"></span>
+                                    Loans
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="bar-chart-2"></span>
+                                    Reports
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="settings"></span>
+                                    Settings
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>Saved reports</span>
+                            <a class="d-flex align-items-center text-muted" href="#">
+                                <span data-feather="plus-circle"></span>
+                            </a>
+                        </h6>
+                        <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="file-text"></span>
+                                    Current month
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span data-feather="file-text"></span>
+                                    Last quarter
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+
+                    @yield('page-content')
+
+                </main>
+            </div>
+        </div>
+        <!-- END SIDEBAR -->
+
+        <!-- START MODAL -->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="logoutModalLabel">Confirm Log out</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to log out?
+                    </div>
+                    <div class="modal-footer">
+                        <form action="{{ route('logout') }}" method="POST">
+                            {{ csrf_field() }}
+
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn btn-primary">Log out</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END MODAL -->
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="{{ url('js/jquery-3.3.1.js') }}" crossorigin="anonymous"></script>
+        <script src="{{ url('js/popper.js') }}" crossorigin="anonymous"></script>
+        <script src="{{ url('js/bootstrap-4.1.0.js') }}" crossorigin="anonymous"></script>
+        <script src="{{ url('js/feather.js') }}"></script>
+
+        <script>
+            feather.replace()
+        </script>
+
+        @yeild('scripts')
+    </body>
+</html>
