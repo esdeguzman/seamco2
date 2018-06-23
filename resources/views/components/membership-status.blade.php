5c7c9dc23be0adfe471ded84404bd850f723b650
Daniel Bajana <it.danielbajana@gmail.com>
2018-06-23 19:19:34 +0800

    Remove head element to membership status component

--- resources/views/components/membership-status.blade.php
+++ resources/views/components/membership-status.blade.php
@@ -1,10 +1,7 @@
-<h6>
-    @if ($slot == 'approved')
-        <span class="badge badge-success">{{ $slot }}</span>
-    @elseif ($slot == 'disapproved')
-        <span class="badge badge-danger">{{ $slot }}</span>
-    @else
-        <span class="badge badge-secondary">{{ $slot }}</span>
-    @endif
-
-</h6>
+@if ($slot == 'approved')
+    <span class="badge badge-success">{{ $slot }}</span>
+@elseif ($slot == 'disapproved')
+    <span class="badge badge-danger">{{ $slot }}</span>
+@else
+    <span class="badge badge-secondary">{{ $slot }}</span>
+@endif
